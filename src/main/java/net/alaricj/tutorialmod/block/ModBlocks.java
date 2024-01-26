0f297e682f0fba3c07b5609fd49421bbbafb02d3
Alaric Jenkins <alaricjenkins@outlook.com>
2024-01-26 15:05:18 -0500

    Added ores for Sapphire

--- src/main/java/net/alaricj/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/alaricj/tutorialmod/block/ModBlocks.java
@@ -2,10 +2,12 @@ package net.alaricj.tutorialmod.block;
 
 import net.alaricj.tutorialmod.TutorialMod;
 import net.alaricj.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,6 +22,13 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE).sound(SoundType.DEEPSLATE)));
+
+    public static final RegistryObject<Block> SAPPHIRE_ORE =
+            registerBlock("sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE).strength(2.5f),
+                    UniformInt.of(3, 5)));
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE =
+            registerBlock("deepslate_sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(3.0f),
+                    UniformInt.of(3, 5)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
