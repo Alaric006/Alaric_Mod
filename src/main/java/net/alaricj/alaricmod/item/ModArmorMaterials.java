64b0412acc395b971968e6c18714162403edfd11
Alaric Jenkins <alaricjenkins@outlook.com>
2024-02-16 19:00:23 -0500

    Added trimable sapphire armor into the mod.

---
+++ src/main/java/net/alaricj/alaricmod/item/ModArmorMaterials.java
@@ -0,0 +1,82 @@
+package net.alaricj.alaricmod.item;
+
+import net.alaricj.alaricmod.TutorialMod;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    SAPPHIRE("sapphire", 26, new int[]{5, 7, 5, 4}, 25, SoundEvents.ARMOR_EQUIP_GOLD, 1f, 0,
+            () -> Ingredient.of(ModItems.SAPPHIRE.get()));
+    private final String name;
+    private final int durabilityMultiplier;
+
+    private final int[] protectionAmmounts;
+
+    private final int enchantmentValue;
+
+    private final SoundEvent equipmentSound;
+
+    private final float toughness;
+
+    private final float knockbackResistance;
+
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static final int[] BASE_DURABILITY =  {11, 16, 15, 13};
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmmounts, int enchantmentValue, SoundEvent equipmentSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmmounts = protectionAmmounts;
+        this.enchantmentValue = enchantmentValue;
+        this.equipmentSound = equipmentSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type type) {
+        return this.BASE_DURABILITY[type.ordinal()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type type) {
+        return this.protectionAmmounts[type.ordinal()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipmentSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return TutorialMod.MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
